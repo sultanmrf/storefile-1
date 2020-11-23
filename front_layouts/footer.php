c50c837318efbe9a055546fb5ffd9a7484b443d5
sultanf <sultanfanody@gmail.com>
2020-11-23 14:56:37 +0330

    add img to footer.php

--- front_layouts/footer.php
+++ front_layouts/footer.php
@@ -1,5 +1,7 @@
-<footer class="bg-dark text-white text-center p-3 o-font-xs position-fixed w-100">
+<footer class="bg-dark text-white text-center p-3 o-font-xs  w-100">
 
     <div class=" text-danger"><?php echo "در این تاریخ و زمان شما وارد سایت شدید " .  date("Y/m/d H:i:s");   ?></div>
         <p class="mb-0">.تمامی حقوق مادی و معنوی این وبسایت به <a href="#">فروشگاه فایل</a> تعلق دارد</p>
+
+    <img src="images/error-404.png" alt="404" width="200" height="200">
     </footer>
